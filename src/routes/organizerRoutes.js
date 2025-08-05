3c67d4eb5ec2031eab167e7268984627d9c301dd
@innovatz-dev-mirza <mirza.vendor.pasti@pegadaian.co.id>
2025-08-05 14:30:39 +0700

    setting JWT token for organizer

--- src/routes/organizerRoutes.js
+++ src/routes/organizerRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getOrganizer, registerOrganizer, loginOrganizer } from "../controllers/organizerController.js";
+import { authenticateJWT } from '../middleware/jwtMiddleware.js';
 
 const router = express.Router();
 
-router.get('/organizers', getOrganizer);
+router.get('/organizers', authenticateJWT, getOrganizer);
 router.post('/register', registerOrganizer);
 router.post('/login', loginOrganizer);
 
