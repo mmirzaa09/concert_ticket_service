26cc813f288eda19f0feefc19e5e51d230f09336
mmirzaa09 <alrasyid.mirza.mm@gmail.com>
2025-11-20 10:39:06 +0700

    update vercel

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,11 +1,18 @@
 import { Pool } from 'pg';
 
+// const dbConnect = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const dbConnect = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 export default dbConnect;
