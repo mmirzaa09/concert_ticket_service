c3af020475d064ec5e8b932ce27510b23c0efce4
mmirzaa09 <alrasyid.mirza.mm@gmail.com>
2025-11-22 20:29:03 +0700

    setting header for vercel

--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -2,7 +2,17 @@ import { jwtVerify } from '../utils/functions.js';
 import * as response from '../utils/responseHandler.js';
 
 export const authenticateJWT = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+    // const token = req.headers['authorization']?.split(' ')[1];
+    const oidcToken = req.headers['x-vercel-oidc-token'];
+    const authHeader = req.headers['authorization'];
+
+    let token;
+
+    if (oidcToken) {
+        token = oidcToken;
+    } else if (authHeader) {
+        token = authHeader.split(' ')[1];
+    }
 
     if (!token) {
         return response.unauthorized(res, 'Access denied. No token provided.');
